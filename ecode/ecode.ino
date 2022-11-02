23ec347929dc0d25845ed18b82b0409ac13d0606
kseniasav86 <115007379+kseniasav86@users.noreply.github.com>
2022-11-02 11:37:54 +0800

    Add files via upload

---
+++ ecode/ecode.ino
@@ -0,0 +1,60 @@
+#define DATA_PIN 2
+#define DASH_DURATION 3
+#define DOT_DURATION 3
+#define DATA_LEVEL LOW
+#define SPACE_LEVEL HIGH
+#define TU 100
+
+String CODES[] = {".-", "--.."};
+char LETTERS[] = {'A', 'Z'};
+int NLETTERS = 2;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(DATA_PIN, OUTPUT);
+  digitalWrite(DATA_PIN, SPACE_LEVEL);
+}
+
+void loop() 
+{
+  if (Serial.available() > 1)
+  {
+    char letter = Serial.read();
+
+    for (int iletter; iletter < NLETTERS; iletter++)
+    {
+      if (letter == LETTERS[iletter])
+      {
+        String code = CODES[iletter];
+      }
+    }
+  }
+}
+
+void send_letter(String code) 
+{
+  int code_len = code.length();
+
+  for (int i = 0; i < code_len; i++)
+  {
+    char symbol = code[i];
+
+    if (symbol == '.')
+    {
+      digitalWrite(DATA_PIN, DATA_LEVEL);
+      delay(TU);
+    }
+
+    if (symbol == '-')
+    {
+      digitalWrite(DATA_PIN, DATA_LEVEL);
+      delay(DASH_DURATION * TU);
+    }
+
+    digitalWrite(DATA_PIN, SPACE_LEVEL);
+    delay(TU);
+  }
+  
+  delay((DASH_DURATION - 1) * TU);
+}
