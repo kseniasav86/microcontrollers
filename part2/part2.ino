90612b8a30dc7280f5f3371b725dc5d68ef0495e
kseniasav86 <115007379+kseniasav86@users.noreply.github.com>
2022-11-09 14:08:23 +0800

    Add files via upload

--- part2/part2.ino
+++ part2/part2.ino
@@ -1,5 +1,3 @@
-
-
 #define led_pin_2 2
 #define led_pin_3 3
 #define led_pin_4 4
@@ -9,12 +7,23 @@
 #define led_pin_8 8
 #define led_pin_9 9
 
+void low()
+{
+  digitalWrite(led_pin_2, LOW);
+  digitalWrite(led_pin_3, LOW);
+  digitalWrite(led_pin_4, LOW);
+  digitalWrite(led_pin_5, LOW);
+  digitalWrite(led_pin_6, LOW);
+  digitalWrite(led_pin_7, LOW);
+  digitalWrite(led_pin_8, LOW);
+  digitalWrite(led_pin_9, LOW);
+}
+
 void setup() 
 { 
   Serial.begin(9600);
  
   while (!Serial) { } 
-
 
   pinMode(led_pin_2, OUTPUT);
   pinMode(led_pin_3, OUTPUT);
@@ -32,18 +41,87 @@ void loop()
 {
   if (Serial.available() > 0)
   {
+    low();
     second_part = Serial.read() - '0';
 
     if (second_part == 1)
     {
-      digitalWrite(led_pin_5, HIGH);
+      digitalWrite(led_pin_9, HIGH);
+      digitalWrite(led_pin_4, HIGH);
+    }
+    else if (second_part == 2)
+    {
+      digitalWrite(led_pin_2, HIGH);
+      digitalWrite(led_pin_3, HIGH);
+      digitalWrite(led_pin_7, HIGH);
+      digitalWrite(led_pin_8, HIGH);
+      digitalWrite(led_pin_9, HIGH);
+    }
+    else if (second_part == 3)
+    {
+      digitalWrite(led_pin_3, HIGH);
+      digitalWrite(led_pin_4, HIGH);
+      digitalWrite(led_pin_7, HIGH);
+      digitalWrite(led_pin_8, HIGH);
+      digitalWrite(led_pin_9, HIGH);
+    }
+    else if (second_part == 4)
+    {
+      digitalWrite(led_pin_4, HIGH);
+      digitalWrite(led_pin_6, HIGH);
+      digitalWrite(led_pin_7, HIGH);
+      digitalWrite(led_pin_9, HIGH);
+    }
+    else if (second_part == 5)
+    {
       digitalWrite(led_pin_3, HIGH);
       digitalWrite(led_pin_4, HIGH);
       digitalWrite(led_pin_6, HIGH);
       digitalWrite(led_pin_7, HIGH);
       digitalWrite(led_pin_8, HIGH);
     }
+    else if (second_part == 6)
+    {
+      digitalWrite(led_pin_2, HIGH);
+      digitalWrite(led_pin_3, HIGH);
+      digitalWrite(led_pin_4, HIGH);
+      digitalWrite(led_pin_6, HIGH);
+      digitalWrite(led_pin_7, HIGH);
+      digitalWrite(led_pin_8, HIGH);
+    }
+    else if (second_part == 7)
+    {
+      digitalWrite(led_pin_4, HIGH);
+      digitalWrite(led_pin_8, HIGH);
+      digitalWrite(led_pin_9, HIGH);
+    }
+    else if (second_part == 8)
+    {
+      digitalWrite(led_pin_2, HIGH);
+      digitalWrite(led_pin_3, HIGH);
+      digitalWrite(led_pin_4, HIGH);
+      digitalWrite(led_pin_6, HIGH);
+      digitalWrite(led_pin_7, HIGH);
+      digitalWrite(led_pin_8, HIGH);
+      digitalWrite(led_pin_9, HIGH);
+    }
+    else if (second_part == 9)
+    {
+      digitalWrite(led_pin_3, HIGH);
+      digitalWrite(led_pin_4, HIGH);
+      digitalWrite(led_pin_6, HIGH);
+      digitalWrite(led_pin_7, HIGH);
+      digitalWrite(led_pin_8, HIGH);
+      digitalWrite(led_pin_9, HIGH);
+    }
+    else if (second_part == 0)
+    {
+      digitalWrite(led_pin_2, HIGH);
+      digitalWrite(led_pin_3, HIGH);
+      digitalWrite(led_pin_4, HIGH);
+      digitalWrite(led_pin_6, HIGH);
+      digitalWrite(led_pin_8, HIGH);
+      digitalWrite(led_pin_9, HIGH);
+    }
   }
-
-  
 }
