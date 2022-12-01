7b4ac831c4bf4ae7ccbdd939d83469a3a563056f
kseniasav86 <115007379+kseniasav86@users.noreply.github.com>
2022-12-01 09:09:21 +0800

    Add files via upload

---
+++ wifi_led_mqtt/Server.h
@@ -0,0 +1,32 @@
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server(80);
+
+void handleRoot()
+{
+String form = "<form action=\"/LED\" method=\"POST\">";
+form = form + "<input type=\"submit\" value=\"Toggle LED\">";
+form = form + "</form>";
+server.send(200, "text/html", form);
+}
+
+void handleLED()
+{
+digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+server.sendHeader("Location", "/");
+server.send(303);
+}
+
+void handleNotFound()
+{
+server.send(404, "test/plain", "404: not found");
+}
+
+void server_init()
+{
+server.on("/", HTTP_GET, handleRoot);
+server.on("/LED", HTTP_POST, handleLED);
+server.onNotFound(handleNotFound);
+server.begin();
+Serial.println("Web Server is ON");
+}
